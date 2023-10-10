12038926659fa037052d6b8c1109b53f21591e99
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-10-10 15:51:23 +0530

    🚧 components

---
+++ apps/next-playground/src/app/Signup/page.tsx
@@ -0,0 +1,13 @@
+import { Signup } from "ui";
+
+import React from 'react'
+
+const SignupPage = () => {
+  return (
+    <div>
+        <Signup/>
+    </div>
+  )
+}
+
+export default SignupPage
--- apps/next-playground/src/app/page.tsx
+++ apps/next-playground/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import {Card} from 'ui'
+import SignupPage from './Signup/page'
 export default function Home() {
   return (
 <>
 <Card title={'Ajaydeep'} children={undefined} href={''}/> 
+<SignupPage />
 </>
   )
 }
